ee3065523f5f6184b100c21ae094ced313b8c1c0
Osvaldo <osvaldoadelatorretec@gmail.com>
2020-06-06 22:06:08 -0500

    Se agrego la interfaz de la ventana de login

--- src/Vista/InicioSeg.java
+++ src/Vista/InicioSeg.java
@@ -1,10 +1,12 @@
-package Vista;
+ package Vista;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 class Login extends JFrame{
 	
@@ -17,63 +19,106 @@ class Login extends JFrame{
 		setSize(400, 300);
 		setLocationRelativeTo(null);
 		setVisible(true);
-		getContentPane().setBackground(new Color(160,111,111));
+		getContentPane().setBackground(new Color(241, 236, 235 ));
 		setResizable(false);
 		
 		JLabel titulo = new JLabel();
 		titulo.setText("Locania");
 		titulo.setFont(new Font("Edwardian Script ITC", Font.BOLD, 60));
 		titulo.setBounds(90, 10, 200, 50);
-		titulo.setForeground(Color.WHITE);
+		titulo.setForeground(new Color(236, 240, 241  ));
 		add(titulo);
 		
 		JLabel subT = new JLabel();
 		subT.setText("Hotel");
 		subT.setFont(new Font("Perpetua Titling MT", Font.BOLD, 16));
 		subT.setBounds(160, 55, 60, 20);
-		subT.setForeground(Color.WHITE);
+		subT.setForeground(new Color(236, 240, 241 ));
 		add(subT);
 		
+		JLabel lT = new JLabel("");
+		lT.setBounds(20, 0, 5, 90);
+		lT.setBackground(new Color(10, 134, 238));
+		lT.setOpaque(true);
+		add(lT);
+		
+		JLabel lT2 = new JLabel("");
+		lT2.setBounds(370, 0, 5, 90);
+		lT2.setBackground(new Color(10, 134, 238));
+		lT2.setOpaque(true);
+		add(lT2);
+		
+		JLabel fT = new JLabel("");
+		fT.setBounds(0, 0, 400, 90);
+		fT.setBackground(new Color(48, 102, 190));
+		fT.setOpaque(true);
+		add(fT);
+		
+		JLabel lvUsuario = new JLabel("Usuario");
+		lvUsuario.setFont(new Font("Perpetua Titling MT", Font.BOLD, 10));
+		lvUsuario.setBounds(85, 100, 100, 20);
+		lvUsuario.setForeground(new Color(56, 53, 52 ));
+		add(lvUsuario);
+		
 		JTextField Usuario = new JTextField(10);
-		Usuario.setText("Usuario");
-		Usuario.setBounds(85, 80, 200, 30);
+		Usuario.setText("");
+		Usuario.setBounds(85, 120, 200, 30);
 		add(Usuario);
 		
-			Usuario.addFocusListener(new FocusListener() {
-				
-				@Override
-				public void focusLost(FocusEvent e) {
-					// TODO Auto-generated method stub
-					
-				}
-				
-				@Override
-				public void focusGained(FocusEvent e) {
-					Usuario.setText("");
-					
-				}
-			});
-		
+		JLabel lvContraseña = new JLabel("Contraseña");
+		lvContraseña.setFont(new Font("Perpetua Titling MT", Font.BOLD, 10));
+		lvContraseña.setBounds(85, 150, 100, 20);
+		lvContraseña.setForeground(new Color(56, 53, 52 ));
+		add(lvContraseña);
 		
 		JPasswordField Contra = new JPasswordField(10);
-		Contra.setText("Contraseña");
+		Contra.setText("");
 		Contra.setEchoChar('*');
-		Contra.setBounds(85, 120, 200, 30);
+		Contra.setBounds(85, 170, 200, 30);
 		add(Contra);
-		Contra.addFocusListener(new FocusListener() {
+		
+		JButton IS = new JButton("Iniciar Sesion");
+		IS.setBounds(85, 210, 200, 30);
+		add(IS);
+		
+		JLabel registrate = new JLabel("Registrate");
+		registrate.setFont(new Font("Perpetua Titling MT", Font.BOLD, 10));
+		registrate.setBounds(85, 250, 70, 20);
+		registrate.setForeground(new Color(56, 53, 52 ));
+		add(registrate);
+		registrate.addMouseListener(new MouseListener() {
 			
 			@Override
-			public void focusLost(FocusEvent e) {
-				// TODO Auto-generated method stub
+			public void mouseReleased(MouseEvent arg0) {
+				registrate.setForeground(new Color(48, 102, 190));
 				
 			}
 			
 			@Override
-			public void focusGained(FocusEvent e) {
-				Contra.setText("");
+			public void mousePressed(MouseEvent arg0) {
+				registrate.setForeground(new Color(133, 193, 233));
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				registrate.setForeground(new Color(56, 53, 52));
+				
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				registrate.setForeground(new Color(48, 102, 190));
+				
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				
+				
 				
 			}
 		});
+		
 	}
 }
 
